ddd62c23733b686ead92363baa66cd6f9ff9f65f
eric88525 <eric885251@gmail.com>
2023-03-07 20:29:59 +0800

    daily: add type casting

--- api/daily.py
+++ api/daily.py
@@ -3,12 +3,13 @@ import requests
 
 API = "http://140.116.245.105:9453/items/"
 
+
 def add_task(user_id, server_id, name, description):
     data = {
-        "created_by": user_id,
-        "name": name,
-        "description": description,
-        "server_id": server_id,
+        "created_by": str(user_id),
+        "name": str(name),
+        "description": str(description),
+        "server_id": str(server_id),
     }
 
     response = requests.post(API + "task", json=data)
