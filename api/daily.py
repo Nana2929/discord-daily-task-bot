dec88a2069286f52eebdf328f923c68f6cb5bdab
eric88525 <eric885251@gmail.com>
2023-03-11 01:01:30 +0800

    api/daily: add get_history and add_history

--- api/daily.py
+++ api/daily.py
@@ -4,6 +4,7 @@ try:
 except:
     from .base import Querier, RequestAdd, RequestDelete, RequestUpdate
 
+from datetime import datetime
 
 def add_task(user_id, server_id, name, description):
     data = {
@@ -25,3 +26,27 @@ def get_task(filter_rules: dict):
         task_querier = task_querier.filter_by(field, "eq", values)
 
     return task_querier.query()
+
+
+def get_history(filter_rules: dict):
+    history_querier = Querier("history")
+
+    for field, values in filter_rules.items():
+        history_querier = history_querier.filter_by(field, "eq", values)
+
+    return history_querier.query()
+
+
+def add_history(user_id, task_id, server_id):
+    data = {
+        "user_id": str(user_id),
+        "task_id": int(task_id),
+        "server_id": str(server_id),
+        "accumulate": 1,
+        "consecutive": 1,
+        "last_check": datetime.now().strftime("%Y-%m-%d"),
+    }
+
+    history_adder = RequestAdd("history")
+
+    return history_adder(**data)
