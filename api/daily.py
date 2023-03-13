4684dba97fe5bf20655a076cea2f8dad77a74620
eric88525 <eric885251@gmail.com>
2023-03-13 17:39:20 +0800

    api/daily: add update_history and get_current_time

--- api/daily.py
+++ api/daily.py
@@ -5,6 +5,10 @@ except:
     from .base import Querier, RequestAdd, RequestDelete, RequestUpdate
 
 from datetime import datetime
+import pytz
+
+def get_current_time():
+    return datetime.now(tz=pytz.UTC)
 
 def add_task(user_id, server_id, name, description):
     data = {
@@ -28,8 +32,11 @@ def get_task(filter_rules: dict):
     return task_querier.query()
 
 
-def get_history(filter_rules: dict):
+def get_history(filter_rules: dict={}):
+
     history_querier = Querier("history")
+
+    history_querier.fields("fields", "task_id.name,task_id.id,user_id.time_zone")
 
     for field, values in filter_rules.items():
         history_querier = history_querier.filter_by(field, "eq", values)
@@ -37,16 +44,25 @@ def get_history(filter_rules: dict):
     return history_querier.query()
 
 
-def add_history(user_id, task_id, server_id):
+def add_history(user_id: str, task_id: int, server_id: str, last_check: datetime, **kwargs):
     data = {
         "user_id": str(user_id),
         "task_id": int(task_id),
         "server_id": str(server_id),
         "accumulate": 1,
         "consecutive": 1,
-        "last_check": datetime.now().strftime("%Y-%m-%d"),
+        "last_check": last_check.strftime("%Y-%m-%d"),
     }
 
     history_adder = RequestAdd("history")
 
     return history_adder(**data)
+
+
+def update_history(id: int, accumulate: int, consecutive: int, last_check: datetime, **kwargs):
+
+    history_updater = RequestUpdate("history")
+    return history_updater(id,
+                           accumulate=int(accumulate),
+                           consecutive=int(consecutive),
+                           last_check=last_check.strftime("%Y-%m-%d")).ok
